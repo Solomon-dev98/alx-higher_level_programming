31548b363595e689607d3ef018b9cc812500c05d
Solomon-dev98 <asapnino01@gmail.com>
2024-03-18 16:48:49 +0100

    feat: add python script that imports a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    # import the add function from the add_0 module
+    from add_0 import add
 
-# import the add function from the add_0 module
-from add_0 import add
-
-a = 1
-b = 2
-
-print("{} + {} = {}".format(a, b, add(a, b)))
-
-
-# check if the script is run directly
-if __name__ == "__0-add__":
-    0-add()
+    a = 1
+    b = 2
+    # print the sum of a + b
+    print("{} + {} = {}".format(a, b, add(a, b)))
