d99cb7ecd11aab1307f26aa6e9e9162d8d099215
Solomon-dev98 <asapnino01@gmail.com>
2024-03-18 16:42:55 +0100

    feat: add python script that imports a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,14 +3,12 @@
 # import the add function from the add_0 module
 from add_0 import add
 
+a = 1
+b = 2
 
-def main():
-    a = 1
-    b = 2
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
 
 
 # check if the script is run directly
-if __name__ == "__main__":
-    main()
+if __name__ == "__0-add__":
+    0-add()
