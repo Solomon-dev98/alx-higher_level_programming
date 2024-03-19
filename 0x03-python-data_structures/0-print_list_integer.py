adcd7d2a42a336e62ca6d8fae240d2dab91a1192
Solomon-dev98 <asapnino01@gmail.com>
2024-03-19 17:59:49 +0100

    feat: add script that prints all integers of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" a function that prints all integers of a list """
+
+
+def print_list_integer(my_list=[]):
+
+    length = len(my_list)
+
+    for i in range(length):
+        print("{}".format(my_list[i]))
