4f00e06fe30c83e4fa83c8a7f9a52031bb2f9ed7
Solomon-dev98 <asapnino01@gmail.com>
2024-03-19 11:23:49 +0100

    feat: added the condition check for if argument is 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Print the number of and list of command line arguments."""
     import sys
 
-    argc_len = len(sys.argv) - 1
-    if (argc_len == 0):
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif argc_len == 1:
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(argc_len))
-        for i in range(argc_len):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
