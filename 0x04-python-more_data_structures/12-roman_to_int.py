d7ac326599e9d3851d3385301abe99c515ad74f8
Solomon-dev98 <asapnino01@gmail.com>
2024-04-09 10:31:54 +0100

    feat: add function that converts a roman numeral to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -14,6 +14,6 @@ def roman_to_int(roman_string):
             result += value
         else:
             result -= value
-            prev_value = value
+        prev_value = value
 
     return result
