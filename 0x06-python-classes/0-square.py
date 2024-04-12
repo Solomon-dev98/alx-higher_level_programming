c1058295f75a9abf93cda8d0d987211e878f9598
Solomon-dev98 <asapnino01@gmail.com>
2024-04-12 15:47:05 +0100

    fix: fixed the requirement error by adding a docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+""" Defines a class that does nothing"""
 
 class Square:
-    """ This is an empty class that defines a square"""
+    """This is an empty class that defines a square"""
     pass
