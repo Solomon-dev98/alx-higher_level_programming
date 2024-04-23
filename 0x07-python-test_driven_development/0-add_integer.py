1db97c50b52e3ef80507705b8c7f07a5ccdc86b4
Solomon-dev98 <asapnino01@gmail.com>
2024-04-23 13:17:42 +0100

    fix: resolve error by checking if a and b is a float

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,14 +7,16 @@ def add_integer(a, b=98):
         function that adds two integers.
 
         Args:
-            a(int or float): an argument to the function.
-            b(int or float): an argument to the function.
+            a(int or float): the first number.
+            b(int or float, optional): the second number. defaults to 98.
 
+        Returns:
+            int: the sum of a and b.
         Raises:
             TypeError: a or b is not an integer.
     """
-    if ((not isinstance(a, int) and not isinstance(b, float))):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
