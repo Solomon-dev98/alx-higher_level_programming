8656aee09b27c3f3e7889cbaba766d18a946df7f
Solomon-dev98 <asapnino01@gmail.com>
2024-04-26 13:47:08 +0100

    feat: add python script that defines a class BaseGeometry with two public instance method

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Implementation of a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """a class with public instance method ``area``, ``integer_validator``."""
+
+    def area(self):
+        """A public instance method that raises an Exception when
+            not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """a public instance that validates a value if it's an integer.
+
+            Args:
+                name(str): a string.
+                value(int): the value to be validated.
+            Raises:
+                TypeError: value is not an integer.
+                ValueError: value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
