ef4b2e049a41d2a8f73a60a3ee05a2b5d3dd5f83
Solomon-dev98 <asapnino01@gmail.com>
2024-04-26 12:49:18 +0100

    Fix: fixed error by changing NotImplementedError to Exception

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,4 +6,4 @@ class BaseGeometry:
     """a class with public instance ``area`` that raises an exception"""
     def area(self):
         """a public instance method"""
-        raise NotImplementedError("area() is not implemented")
+        raise Exception("area() is not implemented")
