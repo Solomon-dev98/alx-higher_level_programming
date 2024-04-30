d4e777145ef1232aee7744d66822816be594eb3f
Solomon-dev98 <asapnino01@gmail.com>
2024-04-30 14:45:57 +0100

    feat: add python script for a file that reads a text file and prints it out to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Implementation of a function ``read_file(filename="")``"""
+
+
+def read_file(filename=""):
+    """a function that reads a text file and prints it to stdout
+
+        Args:
+            filename: the file to be read.
+    """
+
+    with open("my_file_0.txt", 'r', encoding='utf-8') as file:
+        data = file.read()
+        print(data, end='')
