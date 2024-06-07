b8d05765ebc48959e435e8352525e7e3ea6e673e
Solomon-dev98 <asapnino01@gmail.com>
2024-06-07 13:42:26 +0100

    feat: add script that lists all records with score >= 10 from second table

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- a script that lists all records with score >= 10.
+
+-- using the SELECT, WHERE and ORDER BY keyword.
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
