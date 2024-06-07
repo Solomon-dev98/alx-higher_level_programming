61358e324a0e86960715ce86048df0aa420c6570
Solomon-dev98 <asapnino01@gmail.com>
2024-06-07 12:25:31 +0100

    feat: add a script that lists all the table of a database in your MySQL server.

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,10 +1,2 @@
--- a script that lists all the tables of a database.
-
--- SELECT TABLE NAME
-SELECT TABLE_NAME
-
--- use the specified database;
-FROM information_schema.TABLES
-
--- The condition to match the specified database
-WHERE TABLE_SCHEMA = DATABASE();
+-- list all tables of a database on my MySql Server.
+SHOW TABLES;
