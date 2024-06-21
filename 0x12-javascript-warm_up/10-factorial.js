0c5e05ec2725a42e17bed17e152abf1f31c34245
Solomon-dev98 <asapnino01@gmail.com>
2024-06-21 13:00:55 +0100

    feat: add a script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (isNaN(num)) {
+    return 1;
+  }
+
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
